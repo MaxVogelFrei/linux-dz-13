ffb2c50d3de3b14508e5aea83d6f37b2ef83526e
root <root@centos7.lan.traektoria.ru>
2020-02-25 14:07:38 +0300

    dz13

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export BORG_PASSPHRASE="vagrant"
 borg create --list -v --stats --compression zlib,5 borg@192.168.13.11:/home/borg/backup::"etc-{now:%Y-%m-%d_%H:%M:%S}" /etc
+borg prune -v --list --keep-hourly=24 --keep-daily=14 --keep-weekly=5 --keep-monthly=12 borg@192.168.13.11:/home/borg/backup
